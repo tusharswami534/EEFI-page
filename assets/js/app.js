968a524529d3c9c620ab9b3f5ab9c13a57a0dbca
tusharswami534 <tusharswami17@gmail.com>
2024-09-21 16:59:56 +0530

    page complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,16 @@ const navmenu = () => {
     .classList.toggle("translate-x-6");
   document.body.classList.toggle("overflow-hidden");
 };
+let mybutton = document.getElementById("back-to-top");
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
