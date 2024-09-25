426a68cde34336ac220aaab15c54008b0ba6cde9
tusharswami534 <tusharswami17@gmail.com>
2024-09-25 10:45:43 +0530

    Done Project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+      container: {
+        center: true,
+        screens: {
+          sm: "992px",
+          md: "1140px",
+          lg: "1140px",
+          xl: "1140px",
+          "2xl": "1140px",
+        },
+      },
+    },
+  };
